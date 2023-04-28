eedd2a8aa97730d9745d611195fb2fa714fe59de
kjuiop <copying_y@naver.com>
2023-04-28 18:27:13 +0900

    [#1] test: go table test 로 refactoring

--- testing/main_test.go
+++ testing/main_test.go
@@ -4,8 +4,23 @@ import "testing"
 
 func TestMySum(t *testing.T) {
 
-	x := mySum(2, 3)
-	if x != 5 {
-		t.Error("Expected", 5, "Got", x)
+	type test struct {
+		data   []int
+		answer int
 	}
+
+	tests := []test{
+		test{[]int{21, 21}, 42},
+		test{[]int{3, 4, 5}, 12},
+		test{[]int{1, 1}, 2},
+		test{[]int{-1, 0, 1}, 0},
+	}
+
+	for _, v := range tests {
+		x := mySum(v.data...)
+		if x != v.answer {
+			t.Error("Expected", v.answer, "Got", x)
+		}
+	}
+
 }
