4ab4c584a0c0589f2e5c0f61a9674d5041dd4444
kjuiop <copying_y@naver.com>
2023-06-23 09:03:02 +0900

    [#11] feat: map 의 구조체 값이 보존되는지 테스트

---
+++ map_testing/map_structure/preseve_value/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	Name  string // 16 bytes
+	Age   int    // 8 bytes
+	Phone string // 16 bytes
+}
+
+func main() {
+	p := Person{Name: "John", Age: 30}
+
+	fmt.Println("============================================================================")
+	m1 := make(map[string]Person)
+	m1["00000001"] = p
+	fmt.Printf("[01] m1 address : %p,m1 pointer : %p m1 value : %v\n", m1, &m1, m1)
+	one(m1)
+	fmt.Printf("[03] m1 address : %p,m1 pointer : %p m1 value : %v\n", m1, &m1, m1)
+}
+
+func one(m1 map[string]Person) {
+	m1["00000002"] = Person{Name: "Jake", Age: 30}
+	fmt.Printf("[02] m1 address : %p,m1 pointer : %p m1 value : %v\n", m1, &m1, m1)
+}
