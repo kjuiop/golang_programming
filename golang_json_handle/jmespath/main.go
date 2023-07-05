402f23d7a4fba89262c8dd561d8e6313be4bc62c
kjuiop <copying_y@naver.com>
2023-07-05 13:14:43 +0900

    [#15] feat: jmespath 표현식을 사용하여 lookup 함수 구현

--- golang_json_handle/jmespath/main.go
+++ golang_json_handle/jmespath/main.go
@@ -6,6 +6,40 @@ import (
 	"github.com/jmespath/go-jmespath"
 )
 
+const JsonString = `
+{
+	"store" : {
+		"book": [
+			{
+				"category": "reference",
+				"author": "Nigel Rees",
+				"title": "Saying of the Century",
+				"price": 8.95
+			},
+			{
+				"category": "fiction",
+				"author": "Evelyn Waugh",
+				"title": "Sword of Honour",
+				"price": 12.99
+			},
+			{
+				"category": "fiction",
+				"author": "Herman Malvilie",
+				"title": "Moby Dick",
+				"isbn": "0-553-21311-3",
+				"price": 8.99
+			}
+		],
+		"bicycle": {
+			"color": "red",
+			"price": 19.95
+		}
+	},
+	"expensive": 12.43
+}
+
+`
+
 func main() {
 	var jsondata = []byte(`{"foo": {"bar": {"baz" : [0,1,2,3,4]}}}`)
 	var data interface{}
@@ -18,4 +52,37 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(result)
+
+	var jsonData interface{}
+	if err := json.Unmarshal([]byte(JsonString), &jsonData); err != nil {
+		panic(err)
+	}
+	var lookupStrings = []string{
+		"expensive",
+		"store.book[0].price",
+		"store.book[-1].isbn",
+		"store.book[1:2].price",
+		"store.book[?isbn].price",
+		"store.book[?price <`10`].price",
+	}
+
+	for _, str := range lookupStrings {
+		res, err := lookup(jsonData, str)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s => %s\n", str, res)
+	}
+}
+
+func lookup(data interface{}, expression string) (string, error) {
+	res, err := jmespath.Search(expression, data)
+	if err != nil {
+		panic(err)
+	}
+	encoded, err := json.Marshal(res)
+	if err != nil {
+		panic(err)
+	}
+	return string(encoded), nil
 }
