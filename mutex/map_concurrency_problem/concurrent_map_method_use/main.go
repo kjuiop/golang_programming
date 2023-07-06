ae986e20537122841d68575967fc180067aa49fe
kjuiop <copying_y@naver.com>
2023-07-06 17:52:48 +0900

    [#16] feat: concurrent_map 테스트, map 의 struct 을 재생성하지 않고 메서드만 실행시켰을 때에는 에러가 나지 않음

---
+++ mutex/map_concurrency_problem/concurrent_map_method_use/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+)
+
+type Person struct {
+	Name  string
+	Age   int
+	Phone string
+}
+
+func main() {
+
+	testMap := make(map[int]*Person)
+	wg := sync.WaitGroup{}
+	testMap[1] = NewPerson(1)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 1; i <= 50000; i++ {
+			person := testMap[1]
+			person.Age = -1
+			person.printPersonName()
+			fmt.Printf("test 1번 고루틴 %p: %d\n", testMap[1], i)
+		}
+	}()
+
+	go printPersonAge(testMap)
+
+	wg.Wait()
+}
+
+func printPersonAge(testMap map[int]*Person) {
+
+	for {
+		for _, person := range testMap {
+			fmt.Printf("test 2번 고루틴 Person Age : %d\n", person.Age)
+		}
+	}
+}
+
+func (p *Person) printPersonName() {
+	fmt.Printf("test 2번 고루틴 Person Name : %s\n", p.Name)
+}
+
+func NewPerson(i int) *Person {
+	return &Person{
+		Name:  "jake_" + strconv.Itoa(i),
+		Age:   i * 10,
+		Phone: "010-3182-0825",
+	}
+}
