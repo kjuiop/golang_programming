7030aae9014f3ebfed55e833d463dafba61fa3a4
kjuiop <copying_y@naver.com>
2023-07-06 17:30:51 +0900

    [#16] feat: concurrent_map_iteration and map write 에러 발생하는 문제 케이스

---
+++ mutex/map_concurrency_problem/concurrent_map_iteration_map_write/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type Person struct {
+	Name  string
+	Age   int
+	Phone string
+}
+
+func main() {
+
+	testMap := make(map[int]*Person)
+	wg := sync.WaitGroup{}
+	testMap[1] = NewPerson(1)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 1; i <= 50000; i++ {
+			testMap[1] = NewPerson(i)
+			fmt.Printf("test 1번 고루틴 %p: %d\n", testMap[1], i)
+		}
+	}()
+
+	go printPersonAge(testMap)
+
+	wg.Wait()
+}
+
+func printPersonAge(testMap map[int]*Person) {
+
+	for {
+		for _, person := range testMap {
+			fmt.Printf("test 2번 고루틴 Person Age : %d\n", person.Age)
+		}
+
+		time.Sleep(1 * time.Millisecond)
+	}
+}
+
+func NewPerson(i int) *Person {
+	return &Person{
+		Name:  "jake_" + strconv.Itoa(i),
+		Age:   i * 10,
+		Phone: "010-3182-0825",
+	}
+}
