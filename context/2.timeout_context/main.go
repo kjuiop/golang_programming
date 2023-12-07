4152b983f4e8b6ac42cc119a6a81799fd7c144b4
kjuiop <copying_y@naver.com>
2023-12-07 16:59:16 +0900

    [#58] feat: timeout context fail

--- context/2.timeout_context/main.go
+++ context/2.timeout_context/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 	"os/signal"
@@ -10,15 +11,20 @@ import (
 
 func main() {
 
-	go printRoutine()
+	go printRoutine(context.Background())
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
 	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	<-ctx.Done()
+	log.Println("receive sigint signal")
 }
 
-func printRoutine() {
+func printRoutine(ctx context.Context) {
 	i := 0
 	isComplete := false
 
@@ -28,11 +34,14 @@ func printRoutine() {
 
 	for {
 		select {
+		case <-ctx.Done():
+			log.Println("ctx done printRoutine close")
+			return
 		default:
 			i++
 			log.Printf("print Routine : %d\n", i)
 			isComplete = false
-			time.Sleep(3 * time.Second)
+			time.Sleep(1 * time.Second)
 			isComplete = true
 		}
 	}
