2dd330726446b045a7df7e7756cee94f3efec735
kjuiop <copying_y@naver.com>
2023-12-07 16:48:36 +0900

    [#58] feat: signal apply test add

---
+++ context/1.signal_apply/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	go printRoutine()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+}
+
+func printRoutine() {
+	i := 0
+	isComplete := false
+
+	defer func() {
+		log.Printf("is complete print Routine : %v\n", isComplete)
+	}()
+
+	for {
+		select {
+		default:
+			i++
+			log.Printf("print Routine : %d\n", i)
+			isComplete = false
+			time.Sleep(3 * time.Second)
+			isComplete = true
+		}
+	}
+}
