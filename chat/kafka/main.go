ef742d1c29df398f0d9e8977cac6ad0f21e0cdeb
kjuiop <copying_y@naver.com>
2024-08-17 16:46:31 +0900

    chat-kafka : config 기능 구현

--- chat/kafka/main.go
+++ chat/kafka/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"chat-kafka/config"
 	"chat-kafka/network"
+	"flag"
 	"log"
 )
 
+var pathFlag = flag.String("config", "./config.toml", "config set")
+var port = flag.String("port", ":1010", "port set")
+
 func main() {
+	flag.Parse()
+
+	_ = config.NewConfig(*pathFlag)
+
 	n := network.NewServer()
 	if err := n.StartServer(); err != nil {
 		log.Fatalln("fail to start server")
