b4ad1ce550f7b40945b758fbaaa6f3d4ce2b527a
kjuiop <copying_y@naver.com>
2024-08-24 17:08:22 +0900

    chat-kafka : kafka docker-compose install file adD

--- chat/kafka/repository/kafka/root.go
+++ chat/kafka/repository/kafka/root.go
@@ -15,15 +15,18 @@ func NewKafka(cfg *config.Config) (*Kafka, error) {
 	k := &Kafka{cfg: cfg}
 	var err error
 
-	if k.producer, err = kafka.NewProducer(&kafka.ConfigMap{
+	// Kafka 프로듀서 초기화
+	k.producer, err = kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": cfg.Kafka.URL,
 		"client.id":         cfg.Kafka.ClientID,
 		"acks":              "all",
-	}); err != nil {
+	})
+
+	if err != nil {
 		return nil, err
-	} else {
-		return k, nil
 	}
+
+	return k, nil
 }
 
 func (k *Kafka) PublishEvent(topic string, value []byte, ch chan kafka.Event) (kafka.Event, error) {
